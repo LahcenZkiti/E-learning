35955330e29ec92f7c417bffba976769df40164a
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-01 22:39:14 +0100

    added server side

---
+++ E-learning/src/test/java/com/ZkitiDev/Elearning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZkitiDev.Elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
