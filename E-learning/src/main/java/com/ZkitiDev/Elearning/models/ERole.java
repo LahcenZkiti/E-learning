f036cb306bb50a69aa447add8df225cce1cbfccf
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-14 19:28:16 +0100

    edit role file

---
+++ E-learning/src/main/java/com/ZkitiDev/Elearning/models/ERole.java
@@ -0,0 +1,8 @@
+package com.ZkitiDev.Elearning.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ETUDIANT,
+    ROLE_FOURMATEUR,
+    ROLE_ADMIN
+}
--- E-learning/src/main/java/com/ZkitiDev/Elearning/repositories/RoleRepository.java
+++ E-learning/src/main/java/com/ZkitiDev/Elearning/repositories/RoleRepository.java
@@ -1,2 +1,9 @@
-package com.ZkitiDev.Elearning.repositories;public interface RoleRepository {
+package com.ZkitiDev.Elearning.repositories;
+
+import com.ZkitiDev.Elearning.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Integer> {
 }
