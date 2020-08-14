0bbb60380f322042aa4b25787d98d3becde6eb54
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-15 00:48:44 +0100

    added findByEtudiantId to repo

--- E-learning/src/main/java/com/ZkitiDev/Elearning/repositories/EtudiantRepository.java
+++ E-learning/src/main/java/com/ZkitiDev/Elearning/repositories/EtudiantRepository.java
@@ -4,6 +4,9 @@ import com.ZkitiDev.Elearning.models.Etudiant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EtudiantRepository extends JpaRepository<Etudiant, String> {
+    Optional<Etudiant> findByEtudiantId(Long etudiantId);
 }
