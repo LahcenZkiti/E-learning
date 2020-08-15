8c39517357b0b102600c4d10a6b03f7f68146da3
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-08-15 16:50:16 +0100

    edit post requist

--- E-learning/src/main/java/com/ZkitiDev/Elearning/controllers/EtudiantController.java
+++ E-learning/src/main/java/com/ZkitiDev/Elearning/controllers/EtudiantController.java
@@ -1,9 +1,11 @@
 package com.ZkitiDev.Elearning.controllers;
 
+import com.ZkitiDev.Elearning.exception.ResourceAlreadyExistsException;
 import com.ZkitiDev.Elearning.exception.ResourceNotFoundException;
 import com.ZkitiDev.Elearning.models.Etudiant;
 import com.ZkitiDev.Elearning.repositories.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,20 +24,26 @@ public class EtudiantController {
     }
 
     @GetMapping("/etudiants/{id}")
-    public ResponseEntity<Etudiant> getEtudiantById(@PathVariable(value = "id")Long etudiantId)throws ResourceNotFoundException {
-        Etudiant etudiant = etudiantRepository.findByEtudiantId(etudiantId)
+    public ResponseEntity<Etudiant> getEtudiantById(@PathVariable(value = "id")String  etudiantId)throws ResourceNotFoundException {
+        Etudiant etudiant = etudiantRepository.findById(etudiantId)
                 .orElseThrow(()-> new ResourceNotFoundException("There is not etudiant with this id :: "+etudiantId));
         return ResponseEntity.ok().body(etudiant);
     }
 
     @PostMapping("/etudiants")
-    public Etudiant createEtudiant(@Valid @RequestBody Etudiant etudiant){
-        return etudiantRepository.save(etudiant);
+    public ResponseEntity<Etudiant> createEtudiant(@Valid @RequestBody Etudiant etudiant) {
+        try {
+            Etudiant newEtudiant = etudiantRepository.save(etudiant);
+
+            return new  ResponseEntity<>(newEtudiant, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+        }
     }
 
     @PutMapping("/etudiants/{id}")
-    public ResponseEntity<Etudiant> updateEtudiant(@PathVariable(value = "id")Long etudiantId,@Valid @RequestBody Etudiant etudiantDetails)throws ResourceNotFoundException{
-        Etudiant etudiant = etudiantRepository.findByEtudiantId(etudiantId)
+    public ResponseEntity<Etudiant> updateEtudiant(@PathVariable(value = "id")String  etudiantId,@Valid @RequestBody Etudiant etudiantDetails)throws ResourceNotFoundException{
+        Etudiant etudiant = etudiantRepository.findById(etudiantId)
                 .orElseThrow(()-> new ResourceNotFoundException("There is not etudiant with this id ::  "+etudiantId));
 
         etudiant.setFirstname(etudiantDetails.getFirstname());
@@ -43,11 +51,21 @@ public class EtudiantController {
         etudiant.setEmail(etudiantDetails.getEmail());
         etudiant.setAge(etudiantDetails.getAge());
         etudiant.setPhone(etudiantDetails.getPhone());
-        etudiant.setUsernme(etudiantDetails.getUsernme());
+        etudiant.setUsername(etudiantDetails.getUsername());
         etudiant.setPassword(etudiantDetails.getPassword());
 
         final Etudiant updatedEtudiant = etudiantRepository.save(etudiant);
 
         return ResponseEntity.ok(updatedEtudiant);
+    }
+
+    @DeleteMapping("/etudiants")
+    public ResponseEntity<HttpStatus> deleteEtudiant(@PathVariable("id")String etudiantId) {
+        try {
+            etudiantRepository.deleteById(etudiantId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
     }
 }
