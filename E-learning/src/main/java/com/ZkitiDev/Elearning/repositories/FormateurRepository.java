94b285d26c0f17d48924392137f70351355f7fcc
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-10-20 00:43:00 +0100

    added findByUsername & ByEmail

--- E-learning/src/main/java/com/ZkitiDev/Elearning/repositories/FormateurRepository.java
+++ E-learning/src/main/java/com/ZkitiDev/Elearning/repositories/FormateurRepository.java
@@ -1,9 +1,15 @@
 package com.ZkitiDev.Elearning.repositories;
 
+import java.util.Optional;
+
 import com.ZkitiDev.Elearning.models.Formateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FormateurRepository extends JpaRepository<Formateur, String> {
+    
+    Optional<Formateur> findByUsername(String username);
+
+    Optional<Formateur> findByEmail(String email);
 }
